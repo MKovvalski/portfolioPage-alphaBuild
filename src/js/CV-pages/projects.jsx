249888a95d2a3389812384273672346f912ac861
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-03-10 18:29:06 +0100

    building content/styling projects component

--- src/js/CV-pages/projects.jsx
+++ src/js/CV-pages/projects.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Projects extends React.Component {
     render () {
-        return <div>projects działa</div>
+        return <div className = "container">
+                <div className = "projects">
+                    <div className = "col-3">
+                        <div className = "project">
+                            project
+                        </div>
+                    </div>
+                    <div className = "col-3">
+                        <div className = "project">
+                            project
+                        </div>
+                    </div>
+                </div>
+            </div>
     }
 }
 
