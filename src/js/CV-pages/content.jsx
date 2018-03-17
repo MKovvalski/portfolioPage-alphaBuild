3e528a340550dfae6caddf67f274d393d56aeff8
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-03-17 12:42:55 +0100

    modeling MainPage

--- src/js/CV-pages/content.jsx
+++ src/js/CV-pages/content.jsx
@@ -21,7 +21,7 @@ class Content extends React.Component {
                 return <PersonalInfo/>;
                 break;
             default:
-                return <Projects/>;
+                return <MainPage/>;
         }
     };
 
