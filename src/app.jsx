9061b360eb71abef8f4ec205d43a4a0b1b55dd28
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-03-19 09:57:18 +0100

    small changes

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-require('./scss/mobile.scss');
+require('./scss/pc.scss');
 
 //importing components
 import MainComponent from "./js/main-component.jsx";
